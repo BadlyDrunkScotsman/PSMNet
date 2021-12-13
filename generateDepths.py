9af836422264fae2ec39575cf8bdeb49beb36646
ErykSz <Rakietong@gmail.com>
2021-12-13 14:22:59 +0100

    .

--- generateDepths.py
+++ generateDepths.py
@@ -182,6 +182,8 @@ def main():
                     trans_y = float(values[2])
                     trans_z = float(values[1])
 
+
+            print("trans_y: "+str(trans_y))
             imgR_o = translate(imgR_o, trans_x, trans_y, trans_z, cv2)
 
             start_time = time.time()
