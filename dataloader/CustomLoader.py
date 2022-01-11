d8e80ba04dcd453d2f16145cbffb87197b6e2095
ErySzmyt <eszmyt@interia.eu>
2022-01-11 22:14:22 +0100

    Update CustomLoader.py

--- dataloader/CustomLoader.py
+++ dataloader/CustomLoader.py
@@ -125,8 +125,7 @@ class myImageFloder(data.Dataset):
             
         matrix = np.float32(T.flatten()[:6].reshape(2,3))
         width, height = right_img.size
-        right_img = cv2.warpAffine(src=right_img, M=matrix, dsize=(width, height))
-
+        right_img = cv2.warpAffine(src=np.array(right_img), M=matrix, dsize=(width, height))
 
         processed = preprocess.get_transform(augment=False)
 
