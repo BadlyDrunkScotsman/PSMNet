4d1a9957e47df0ce49d3493dc7415da4465697db
ErySzmyt <eszmyt@interia.eu>
2022-01-12 01:07:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -150,14 +150,14 @@ def test(imgL, imgR, disp_true):
 
     with torch.no_grad():
         output3 = model(imgL, imgR)
+        output3 = torch.squeeze(output3, 0)
 
-    print(output3.shape)
+    #print(output3.shape)
 
     if top_pad != 0:
         img = output3[:, top_pad:, :]
     else:
         img = output3
-
 
     im_m = img[mask]
     d_t = disp_true[mask]
