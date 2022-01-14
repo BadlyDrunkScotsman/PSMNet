eda0e9cdbf0732c67e39e65d747653d6ee96edb0
ErySzmyt <eszmyt@interia.eu>
2022-01-14 01:06:42 +0100

    Update generateDepths.py

--- generateDepths.py
+++ generateDepths.py
@@ -161,9 +161,9 @@ for batch_idx, (imgL, imgR, disp_L, r_path) in enumerate(TestImgLoader):
 
     curr_gt_outpath = outpath + dirname + "_gt/"
 
-    if(os.path.exists(curr_outpath)):
+    if(not os.path.exists(curr_outpath)):
         os.mkdir(curr_outpath)
-    if(os.path.exists(curr_gt_outpath)):
+    if(not os.path.exists(curr_gt_outpath)):
         os.mkdir(curr_gt_outpath)
 
     img.save(os.path.join(curr_outpath, img_name))
